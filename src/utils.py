381c4cb0655e5fc8c687cdc3994d02af0f2fd449
Luca Tramonti <luca.tramonti@outlook.it>
2025-01-15 01:57:19 +0100

    Add present value functions to utils and update test notebook

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,10 @@ def compounded_interest(cash: float, interest_rate: float, periods: float):
     return cash * (1 + interest_rate)**periods
 
 
+def present_value_single(cash: float, discount_rate: float, periods: float):
+    return compounded_interest(cash, discount_rate, -periods)
+
+
+
+def present_value_cash_flow(cash_flows: np.array, discount_rate: float, periods: np.array):
+    return cash_flows / ((1 + discount_rate)**periods)
